56964a6f046666911256e6c63c8344c2ea5fdaac
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-03-31 17:34:21 -0600

    Done

---
+++ Helpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Hex
+{
+    internal static class Helpers
+    {
+        public static string Reverse(this string str)
+        {
+            StringBuilder strB = new StringBuilder();
+
+            for (int i = str.Length - 1; i >= 0; i--)
+                strB.Append(str[i]);
+
+            return strB.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Hex;
+using System.Text.RegularExpressions;
+
+string txt;
+
+Console.WriteLine("Escribe la palabra que quieras poner sus caracteres al revés");
+txt = Console.ReadLine();
+
+txt = Regex.Replace(txt, @"\s+", " ").Trim();
+
+var words = txt.Split(' ');
+
+foreach (var word in words)
+{
+    string txtReverse = Helpers.Reverse($"{word} ");
+
+    Console.Write(txtReverse);
+}
